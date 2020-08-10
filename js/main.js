7d7ef1e2bacd535140dce40696a5ca7bc888db7e
Joschka Cesar <cesar@posteo.de>
2020-08-10 17:46:25 +0200

    Add text-manipulation

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 document.querySelector('#create-task__button-submit-input').addEventListener('click', addItem);
-
+let counter = 0;
 function addItem(){
+    counter++;
     let todoTemplate = document.querySelector('.todo-template');
     let newTask = todoTemplate.cloneNode(true);
+    newTask.id = "todo"+ counter;
+    console.log(newTask.id);
     todoTemplate.after(newTask);
     let input = document.getElementById("create-task__task-input").value;
-    document.getElementById("create-task__task-input").value = '';
-    
+    console.log(newTask.querySelectorAll(":scope > .todo-template__text").textContent);
+    /*document.getElementsByClassName("todo-template__text")[counter].textContent = input;*/
+
 }
-
-
 
 function editItem(){
 
