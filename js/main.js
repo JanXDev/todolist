67486747e88148def772a916a55f1b0038a36bde
Joschka Cesar <cesar@posteo.de>
2020-08-10 15:52:12 +0200

    Add ToDo functionality
    New ToDos can be added via a button

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
-
+document.querySelector('#create-task__button-submit-input').addEventListener('click', addItem);
 
 function addItem(){
-
-};
+    let todoTemplate = document.querySelector('.todo-template');
+    let newTask = todoTemplate.cloneNode(true);
+    todoTemplate.after(newTask);
+}
 
 
 
 function editItem(){
 
 
-};
+}
 
 
 
 function deleteItem(){
 
 
-};
+}
 
 
