dd59559302cff08fa1d715a5f65fadc57f26844a
Joschka Cesar <cesar@posteo.de>
2020-08-11 16:21:35 +0200

    Resolve merge conflict

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-document
-  .querySelector("#create-task__button-submit-input")
-  .addEventListener("click", addItem);
-
-function addItem() {
-  let todoTemplate = document.querySelector(".todo-template");
-  let newTask = todoTemplate.cloneNode(true);
-  todoTemplate.after(newTask);
-
-  let input = document.getElementById("create-task__task-input").value;
-  document.getElementById("create-task__task-input").value = "";
-}
-
-function editItem() {}
-
-function deleteItem() {}
-=======
 let counter = 0;
 
 const addItem = () => {
