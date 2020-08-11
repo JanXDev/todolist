c9e009d4baad350e35a6ea30720d591e5d92dc2b
Thomas Rinke <thomasrinke@gmx.de>
2020-08-11 15:21:26 +0200

    I changed something but deleted it afterwards

--- js/main.js
+++ js/main.js
@@ -1,24 +1,16 @@
-document.querySelector('#create-task__button-submit-input').addEventListener('click', addItem);
+document
+  .querySelector("#create-task__button-submit-input")
+  .addEventListener("click", addItem);
 
-function addItem(){
-    let todoTemplate = document.querySelector('.todo-template');
-    let newTask = todoTemplate.cloneNode(true);
-    todoTemplate.after(newTask);
+function addItem() {
+  let todoTemplate = document.querySelector(".todo-template");
+  let newTask = todoTemplate.cloneNode(true);
+  todoTemplate.after(newTask);
 
-    let input = document.getElementById("create-task__task-input").value;
-    document.getElementById("create-task__task-input").value = '';
+  let input = document.getElementById("create-task__task-input").value;
+  document.getElementById("create-task__task-input").value = "";
 }
 
+function editItem() {}
 
-
-function editItem(){
-
-
-}
-
-
-
-function deleteItem(){
-
-
-}
+function deleteItem() {}
