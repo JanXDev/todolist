0558a5760cdd423f6a6d46b32023474b2bda9121
Joschka Cesar <cesar@posteo.de>
2020-08-11 17:55:14 +0200

    Add Edit and Delete functionality
    Additional functionality: delete fucntion, edit function, change default visibility

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
+//Global Counter for "To Do" IDs
 let counter = 0;
 
-const addItem = () => {
+// Make the template and the Edit section invisible by default
+document.getElementsByClassName("todo-template")[0].style.display = 'none';
+document.getElementsByClassName("todolist-container__edit-task")[0].style.display = 'none';
 
+
+const addItem = () => {
     // Copy the Template
     let todoTemplate = document.querySelector('.todo-template');
     let newTask = todoTemplate.cloneNode(true);
@@ -11,6 +16,8 @@ const addItem = () => {
     let todoID = counter;
     newTask.id = "todo"+ todoID;
     todoTemplate.after(newTask);
+    newTask.style.display = "flex";
+    console.log(newTask);
 
     //Parse the Content of the Input into the To Do, then delete the Input
     let todoInput = document.getElementById("create-task__task-input").value;
@@ -18,17 +25,31 @@ const addItem = () => {
     document.querySelector("#create-task__task-input").value = '';
 
     // Add Event Listeners for the Buttons
-    newTask.querySelectorAll(":scope > button")[0].addEventListener("click", editItem.bind(null, todoID));
-    newTask.querySelectorAll(":scope > button")[1].addEventListener("click", deleteItem.bind(null, todoID));
+    newTask.querySelectorAll("button")[0].addEventListener("click", editItem.bind(null, todoID));
+    newTask.querySelectorAll("button")[1].addEventListener("click", deleteItem.bind(null, todoID));
 }
 
 const editItem = (toDoID) => {
-    console.log(`edit ${toDoID}`);
+    document.getElementsByClassName("todolist-container__edit-task")[0].style.display = 'flex';
+    document.querySelector("#todo" + toDoID).querySelector(".todo-template__text").textContent =
+        document.getElementById("edit-task__task-edit").value;
+    /*
+    let inputField = document.getElementById("edit-task__task-edit").value;
+    let taskContainer = document.querySelector("#todo" + toDoID);
+    let textContainer = taskContainer.querySelector(".todo-template__text");
+    textContainer.textContent = inputField; */
 }
 
 const deleteItem = (toDoID) => {
-    console.log(`delete ${toDoID}`);
+    document.querySelector("#todo" + toDoID).remove();
+}
+
+const submitChanges = () => {
+    document.getElementsByClassName("todolist-container__edit-task")[0].style.display = 'none';
 }
 
 // "To Do" Button Event Listener
 document.querySelector('#create-task__button-submit-input').addEventListener('click', addItem);
+
+// "Submit Changes" Button Event Listener
+document.querySelector('#edit-task__button-submit-edit').addEventListener('click', submitChanges);
