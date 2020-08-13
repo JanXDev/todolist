9fba0f9321546f7f1b042efa2636bfa5583648e1
Thomas Rinke <thomasrinke@gmx.de>
2020-08-13 18:50:58 +0200

    major design changes

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ let easterEgg = 0;
 // Make the template and the Edit section invisible by default
 document.getElementsByClassName("todo-template")[0].style.display = "none";
 // Bottom section can be deleted. Editing now works in the task itself.
-document.getElementsByClassName(
+/*document.getElementsByClassName(
   "todolist-container__edit-task"
-)[0].style.display = "none";
+)[0].style.display = "none";*/
 
 const addItem = () => {
   // Copy the Template
@@ -15,7 +15,6 @@ const addItem = () => {
   if (todoInput != "") {
     let todoTemplate = document.querySelector(".todo-template");
     let newTask = todoTemplate.cloneNode(true);
-
 
     // Assign a new ID to the To Do
     counter++;
@@ -31,11 +30,11 @@ const addItem = () => {
 
     // Add Event Listeners for the Buttons
     newTask
-        .querySelectorAll("button")[0]
-        .addEventListener("click", editItem.bind(null, todoID));
+      .querySelectorAll("button")[0]
+      .addEventListener("click", editItem.bind(null, todoID));
     newTask
-        .querySelectorAll("button")[1]
-        .addEventListener("click", deleteItem.bind(null, todoID));
+      .querySelectorAll("button")[1]
+      .addEventListener("click", deleteItem.bind(null, todoID));
   } else {
     easterEgg++;
     // alert("Please enter a valid string");
@@ -48,13 +47,13 @@ const addItem = () => {
 
 const editItem = (toDoID) => {
   let taskDiv = document.querySelector("#todo" + toDoID);
-  let taskContent = taskDiv.querySelectorAll(".todo-template__text")[0]
+  let taskContent = taskDiv.querySelectorAll(".todo-template__text")[0];
   let buttonFunction = taskDiv.querySelectorAll("button")[0];
 
   // This checks which functionality the Button should perform
   if (buttonFunction.textContent === "Edit") {
     buttonFunction.textContent = "Save";
-    taskContent.style.display = 'none';
+    taskContent.style.display = "none";
     let inputField = document.createElement("input");
     inputField.value = taskContent.textContent;
     console.log(taskContent);
@@ -62,7 +61,7 @@ const editItem = (toDoID) => {
   } else {
     buttonFunction.textContent = "Edit";
     taskContent.textContent = taskDiv.querySelectorAll("input")[0].value;
-    taskContent.style.display = 'block';
+    taskContent.style.display = "block";
     taskDiv.querySelectorAll("input")[0].remove();
   }
 };
